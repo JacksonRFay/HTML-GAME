c840734900ba04d9e8e6be1bd394fb285234df31
JacksonRFay <119723082+JacksonRFay@users.noreply.github.com>
2023-12-23 00:38:51 -0800

    Add files via upload

---
+++ Donavelli Website/JS/main.js
@@ -0,0 +1,106 @@
+
+// Selectors
+
+const toDoInput = document.querySelector('.todo-input');
+const toDoBtn = document.querySelector('.todo-btn');
+const toDoList = document.querySelector('.todo-list');
+document.addEventListener('DOMContentLoaded', function() {
+    const bodyElement = document.querySelector('body');
+    bodyElement.classList.add('standard-theme');
+});
+
+const lightTheme = document.querySelector('.light-theme');
+const darkerTheme = document.querySelector('.darker-theme');
+
+
+// Event Listeners
+
+toDoBtn.addEventListener('click', addToDo);
+toDoList.addEventListener('click', deletecheck);
+document.addEventListener("DOMContentLoaded", getTodos);
+standardTheme.addEventListener('click', () => changeTheme('standard'));
+lightTheme.addEventListener('click', () => changeTheme('light'));
+darkerTheme.addEventListener('click', () => changeTheme('darker'));
+
+// Check if one theme has been set previously and apply it (or std theme if not found):
+let savedTheme = localStorage.getItem('savedTheme');
+savedTheme === null ?
+    changeTheme('standard')
+    : changeTheme(localStorage.getItem('savedTheme'));
+
+// Functions;
+function addToDo(event) {
+    // Prevents form from submitting / Prevents form from relaoding;
+    event.preventDefault();
+
+    // toDo DIV;
+    const toDoDiv = document.createElement("div");
+    toDoDiv.classList.add('todo', `${savedTheme}-todo`);
+
+    // Create LI
+    const newToDo = document.createElement('li');
+    if (toDoInput.value === '') {
+            alert("You must write something!");
+        } 
+    else {
+        // newToDo.innerText = "hey";
+        newToDo.innerText = toDoInput.value;
+        newToDo.classList.add('todo-item');
+        toDoDiv.appendChild(newToDo);
+
+        // Adding to local storage;
+        savelocal(toDoInput.value);
+
+        // check btn;
+        const checked = document.createElement('button');
+        checked.innerHTML = '<i class="fas fa-check"></i>';
+        checked.classList.add('check-btn', `${savedTheme}-button`);
+        toDoDiv.appendChild(checked);
+        // delete btn;
+        const deleted = document.createElement('button');
+        deleted.innerHTML = '<i class="fas fa-trash"></i>';
+        deleted.classList.add('delete-btn', `${savedTheme}-button`);
+        toDoDiv.appendChild(deleted);
+
+        // Append to list;
+        toDoList.appendChild(toDoDiv);
+
+        // CLearing the input;
+        toDoInput.value = '';
+    }
+
+}   
+
+
+function deletecheck(event){
+
+    // console.log(event.target);
+    const item = event.target;
+
+    // delete
+    if(item.classList[0] === 'delete-btn')
+    {
+        // item.parentElement.remove();
+        // animation
+        item.parentElement.classList.add("fall");
+
+        //removing local todos;
+        removeLocalTodos(item.parentElement);
+
+        item.parentElement.addEventListener('transitionend', function(){
+            item.parentElement.remove();
+        })
+    }
+
+    // check
+    if(item.classList[0] === 'check-btn')
+    {
+        item.parentElement.classList.toggle("completed");
+    }
+
+
+}
+
+
+
+
